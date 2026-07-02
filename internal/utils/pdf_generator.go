3b184aecca78b1b31be992b6533df4b211c6a622
APermata7 <auliapermatakumala@gmail.com>
2026-07-02 20:32:45 +0700

    feat: generate PDF surat cuti/dispen/darurat

---
+++ internal/utils/pdf_generator.go
@@ -0,0 +1,393 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/jung-kurt/gofpdf/v2"
+)
+
+type PDFData struct {
+	CompanyName    string
+	CompanyAddress string
+	CompanyPhone   string
+	CompanyEmail   string
+	LogoPath       string
+
+	Jenis string
+
+	Nama        string
+	Divisi      string
+	Unit        string
+	Jabatan     string
+	LamaCuti    int
+	TanggalCuti string
+	AlasanCuti  string
+
+	JumlahCutiTahun      int
+	CutiDilaksanakan     int
+	CutiAkanDilaksanakan int
+	SisaCuti             int
+
+	DisetujuiSelama int
+	MulaiTanggal    string
+	Tahun           int
+	TanggalSekarang time.Time
+
+	NamaPemohon string
+	NamaAtasan  string
+	NamaHRD     string
+
+	Status  string
+	Catatan string
+}
+
+func addHeader(pdf *gofpdf.Fpdf, data PDFData) {
+	logoSize := 24.0
+	startY := 5.0
+
+	if data.LogoPath != "" {
+		pdf.Image(data.LogoPath, 18, startY, logoSize, logoSize, false, "", 0, "")
+	}
+
+	textStartY := startY + 4.0
+
+	textX := 15.0 + logoSize + 7
+
+	pdf.SetXY(textX, textStartY)
+	pdf.SetFont("Helvetica", "B", 24)
+	pdf.SetTextColor(0, 0, 255)
+	pdf.CellFormat(105, 9, "KOPEGTEL MALANG", "", 0, "L", false, 0, "")
+
+	rightX := 150.0
+	pdf.SetXY(rightX, textStartY)
+	pdf.SetFont("Helvetica", "", 10)
+	pdf.SetTextColor(0, 0, 255)
+	pdf.CellFormat(45, 4.5, "Badan Hukum :", "", 1, "L", false, 0, "")
+	pdf.SetXY(rightX, textStartY+4.5)
+	pdf.CellFormat(45, 4.5, "5538/BH/II/1983", "", 1, "L", false, 0, "")
+	pdf.SetXY(rightX, textStartY+9)
+	pdf.CellFormat(45, 4.5, "Tgl. 28 September 1983", "", 1, "L", false, 0, "")
+
+	pdf.SetXY(textX, textStartY+10)
+	pdf.SetFont("Helvetica", "B", 14)
+	pdf.SetTextColor(0, 0, 255)
+	pdf.CellFormat(105, 8, "Koperasi Pegawai PT. Telkom Malang", "", 1, "L", false, 0, "")
+
+	pdf.SetY(34)
+	pdf.SetTextColor(0, 0, 0)
+
+	pdf.SetDrawColor(255, 165, 0)
+	pdf.SetLineWidth(0.8)
+	pdf.Line(18, 34, 192, 34)
+	pdf.SetLineWidth(0.3)
+
+	pdf.Ln(8)
+}
+
+func addFooter(pdf *gofpdf.Fpdf) {
+	pdf.SetY(-32)
+
+	pdf.SetDrawColor(255, 165, 0)
+	pdf.SetLineWidth(0.8)
+	pdf.Line(18, pdf.GetY(), 192, pdf.GetY())
+	pdf.SetLineWidth(0.3)
+
+	pdf.SetY(-30)
+	pdf.SetFont("Helvetica", "B", 11)
+	pdf.SetTextColor(0, 0, 255)
+	pdf.CellFormat(0, 4, "Alamat: Jl. Ahmad Yani 11 Malang - Jawa Timur | Telepon: 0341 479881 | Faximile: 0341 499553", "", 1, "C", false, 0, "")
+	pdf.SetY(-25)
+	pdf.SetFont("Helvetica", "B", 11)
+	pdf.CellFormat(0, 4, "Email: kopegtel1malang@gmail.com", "", 1, "C", false, 0, "")
+}
+
+func generateBodyCuti(pdf *gofpdf.Fpdf, data PDFData) {
+	pdf.SetY(42)
+	pdf.SetFont("Helvetica", "B", 15)
+	pdf.SetTextColor(0, 0, 0)
+	pdf.CellFormat(0, 8, "PERMOHONAN / LAPORAN CUTI TAHUNAN", "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 0.5, "", "", 1, "", false, 0, "")
+
+	pdf.Ln(6)
+
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.CellFormat(0, 6.5, "DATA PEGAWAI", "", 1, "L", false, 0, "")
+	pdf.Ln(2)
+
+	leftLabel := 30.0
+	leftValue := 55.0
+	lineHeight := 5.5
+	indent := 8.0
+
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "1) Nama")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Nama)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "2) Divisi")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Divisi)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "3) Unit")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Unit)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "4) Jabatan")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Jabatan)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "5) Lama Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, fmt.Sprintf(": %d hari", data.LamaCuti))
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "6) Tanggal Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.TanggalCuti)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "7) Alasan Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.MultiCell(0, lineHeight, ": "+data.AlasanCuti, "", "L", false)
+	pdf.Ln(6)
+
+	if data.Jenis == "CUTI" || data.Jenis == "DARURAT" {
+		pdf.SetFont("Helvetica", "B", 12)
+		pdf.CellFormat(0, 6.5, "CATATAN SDM KOPEGTEL", "", 1, "L", false, 0, "")
+		pdf.Ln(2)
+
+		pdf.SetFont("Helvetica", "", 12)
+		pdf.SetX(18)
+
+		indent := 8.0
+		labelWidth := 85.0
+		valueWidth := 70.0
+
+		pdf.SetX(18 + indent)
+		pdf.Cell(labelWidth, lineHeight, fmt.Sprintf("- Jumlah cuti pada tahun %d", data.Tahun))
+		pdf.SetX(18 + indent + labelWidth)
+		pdf.CellFormat(valueWidth, lineHeight, fmt.Sprintf(": %d hari", data.JumlahCutiTahun), "", 1, "L", false, 0, "")
+
+		pdf.SetX(18 + indent)
+		pdf.Cell(labelWidth, lineHeight, fmt.Sprintf("- Cuti yang telah dilaksanakan tahun %d", data.Tahun))
+		pdf.SetX(18 + indent + labelWidth)
+		pdf.CellFormat(valueWidth, lineHeight, fmt.Sprintf(": %d hari", data.CutiDilaksanakan), "", 1, "L", false, 0, "")
+
+		pdf.SetX(18 + indent)
+		pdf.Cell(labelWidth, lineHeight, fmt.Sprintf("- Cuti yang akan dilaksanakan tahun %d", data.Tahun))
+		pdf.SetX(18 + indent + labelWidth)
+		pdf.CellFormat(valueWidth, lineHeight, fmt.Sprintf(": %d hari", data.CutiAkanDilaksanakan), "", 1, "L", false, 0, "")
+
+		pdf.SetX(18 + indent)
+		pdf.Cell(labelWidth, lineHeight, fmt.Sprintf("- Sisa cuti tahun %d", data.Tahun))
+		pdf.SetX(18 + indent + labelWidth)
+		pdf.CellFormat(valueWidth, lineHeight, fmt.Sprintf(": %d hari", data.SisaCuti), "", 1, "L", false, 0, "")
+
+		pdf.Ln(6)
+	}
+}
+
+func generateBodyDispen(pdf *gofpdf.Fpdf, data PDFData) {
+	pdf.SetY(42)
+	pdf.SetFont("Helvetica", "B", 15)
+	pdf.SetTextColor(0, 0, 0)
+	pdf.CellFormat(0, 8, "PERMOHONAN / LAPORAN DISPENSASI", "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 0.5, "", "", 1, "", false, 0, "")
+
+	pdf.Ln(6)
+
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.CellFormat(0, 6.5, "DATA PEGAWAI", "", 1, "L", false, 0, "")
+	pdf.Ln(2)
+
+	leftLabel := 30.0
+	leftValue := 55.0
+	lineHeight := 6.0
+	indent := 8.0
+
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "1) Nama")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Nama)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "2) Divisi")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Divisi)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "3) Unit")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Unit)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "4) Jabatan")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.Jabatan)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "5) Lama Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, fmt.Sprintf(": %d hari", data.LamaCuti))
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "6) Tanggal Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.TanggalCuti)
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "7) Alasan Cuti")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.MultiCell(0, lineHeight, ": "+data.AlasanCuti, "", "L", false)
+	pdf.Ln(6)
+}
+
+func addKeputusan(pdf *gofpdf.Fpdf, data PDFData) {
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.CellFormat(0, 6.5, "KEPUTUSAN PEJABAT YANG BERWENANG", "", 1, "L", false, 0, "")
+	pdf.Ln(2)
+
+	leftLabel := 35.0
+	leftValue := 55.0
+	lineHeight := 6.0
+	indent := 8.0
+
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "Disetujui selama")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, fmt.Sprintf(": %d hari kerja", data.DisetujuiSelama))
+	pdf.Ln(lineHeight)
+
+	pdf.SetXY(18+indent, pdf.GetY())
+	pdf.Cell(leftLabel, lineHeight, "Mulai tanggal")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.Cell(leftValue, lineHeight, ": "+data.MulaiTanggal)
+	pdf.Ln(lineHeight + 12)
+
+	pdf.SetX(145)
+	pdf.CellFormat(0, 6.5, fmt.Sprintf("Malang, %s", data.TanggalSekarang.Format("02 January 2006")), "", 1, "L", false, 0, "")
+	pdf.Ln(4)
+
+	x := 35.0
+	y := pdf.GetY()
+
+	pdf.SetDrawColor(0, 0, 0)
+
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.SetXY(x, y)
+	pdf.CellFormat(40, 6.5, "Mengetahui HRD", "", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+18)
+	pdf.CellFormat(40, 20, "", "B", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+39)
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.CellFormat(40, 6, data.NamaHRD, "", 0, "C", false, 0, "")
+
+	x += 52
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.SetXY(x, y)
+	pdf.CellFormat(40, 6.5, "Menyetujui Atasan", "", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+18)
+	pdf.CellFormat(40, 20, "", "B", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+39)
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.CellFormat(40, 6, data.NamaAtasan, "", 0, "C", false, 0, "")
+
+	x += 52
+	pdf.SetFont("Helvetica", "B", 12)
+	pdf.SetXY(x, y)
+	pdf.CellFormat(40, 6.5, "Pemohon", "", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+18)
+	pdf.CellFormat(40, 20, "", "B", 0, "C", false, 0, "")
+	pdf.SetXY(x, y+39)
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.CellFormat(40, 6, data.NamaPemohon, "", 0, "C", false, 0, "")
+
+	pdf.Ln(22)
+
+	yAfterTTD := pdf.GetY()
+
+	pdf.SetY(yAfterTTD - 4)
+
+	pdf.SetFont("Helvetica", "I", 10)
+	pdf.CellFormat(0, 5, "Catatan:", "", 1, "L", false, 0, "")
+	pdf.SetFont("Helvetica", "I", 8)
+	if data.Jenis == "CUTI" || data.Jenis == "DARURAT" {
+		pdf.CellFormat(0, 4.5, "1. Form cuti bisa dicetak di masing-masing unit.", "", 1, "L", false, 0, "")
+		pdf.CellFormat(0, 4.5, "2. Cuti yang tidak diketahui HRD KOPEGTEL Malang maka dianggap mangkir.", "", 1, "L", false, 0, "")
+	} else {
+		pdf.CellFormat(0, 4.5, "1. Form dispensasi bisa dicetak di masing-masing unit.", "", 1, "L", false, 0, "")
+		pdf.CellFormat(0, 4.5, "2. Dispensasi yang tidak diketahui HRD KOPEGTEL Malang maka dianggap mangkir.", "", 1, "L", false, 0, "")
+	}
+}
+
+func GeneratePDFCuti(data PDFData) ([]byte, error) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetTitle("Surat Cuti Tahunan", true)
+	pdf.SetAuthor("Sakti Apps - KOPEGTEL", true)
+	pdf.SetMargins(18, 18, 18)
+	pdf.AddPage()
+
+	addHeader(pdf, data)
+	generateBodyCuti(pdf, data)
+	addKeputusan(pdf, data)
+	addFooter(pdf)
+
+	var buf bytes.Buffer
+	err := pdf.Output(&buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func GeneratePDFDispensasi(data PDFData) ([]byte, error) {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetTitle("Surat Dispensasi", true)
+	pdf.SetAuthor("Sakti Apps - KOPEGTEL", true)
+	pdf.SetMargins(18, 18, 18)
+	pdf.AddPage()
+
+	addHeader(pdf, data)
+	generateBodyDispen(pdf, data)
+	addKeputusan(pdf, data)
+	addFooter(pdf)
+
+	var buf bytes.Buffer
+	err := pdf.Output(&buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func GeneratePDF(data PDFData) ([]byte, error) {
+	switch data.Jenis {
+	case "DISPENSASI":
+		return GeneratePDFDispensasi(data)
+	case "DARURAT":
+		return GeneratePDFCuti(data)
+	default:
+		return GeneratePDFCuti(data)
+	}
+}
