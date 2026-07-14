8d1f73b58ff9215bbee38135a51464ae7f487283
APermata7 <auliapermatakumala@gmail.com>
2026-07-15 00:12:57 +0700

    feat: tambahkan domain libur untuk manajemen hari libur

--- internal/domain/libur.go
+++ internal/domain/libur.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Libur struct {
-	ID        string    `json:"id"`
-	Tanggal   string    `json:"tanggal"`
-	Nama      string    `json:"nama"`
-	Jenis     string    `json:"jenis"`
-	Aktif     bool      `json:"aktif"`
-	Sumber    string    `json:"sumber"`
-	DibuatPada time.Time `json:"dibuat_pada"`
+	ID             string    `json:"id"`
+	Tanggal        time.Time `json:"tanggal"`
+	Nama           string    `json:"nama"`
+	Jenis          string    `json:"jenis"`
+	Aktif          bool      `json:"aktif"`
+	Sumber         string    `json:"sumber"`
+	DibuatPada     time.Time `json:"dibuat_pada"`
 	DiperbaruiPada time.Time `json:"diperbarui_pada"`
 }
 
@@ -17,10 +17,10 @@ type CreateLiburRequest struct {
 	Tanggal string `json:"tanggal"`
 	Nama    string `json:"nama"`
 	Jenis   string `json:"jenis"`
+	Sumber  string `json:"sumber"`
 }
 
 type UpdateLiburRequest struct {
 	Nama  string `json:"nama"`
 	Jenis string `json:"jenis"`
-	Aktif bool   `json:"aktif"`
 }
