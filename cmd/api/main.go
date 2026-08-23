7e6deec660f6a65d8e40ef34a1e38d21c1594fd1
APermata7 <auliapermatakumala@gmail.com>
2026-08-23 22:58:16 +0700

    fix(cors): tambahkan localhost untuk development FE

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -118,7 +118,7 @@ func main() {
 
     app.Use(logger.New())
     app.Use(cors.New(cors.Config{
-        AllowOrigins: "https://adminsakti.kopegtelmalang.co.id",
+        AllowOrigins: "https://adminsakti.kopegtelmalang.co.id,http://127.0.0.1:5173,http://localhost:5173",
         AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
         AllowHeaders: "Origin, Content-Type, Accept, Authorization",
     }))
